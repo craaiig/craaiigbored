ebb9af27ab8fc6e6a4d87a93eb058bdeefc998e5
Craig Chilcott <cjc244@gmail.com>
2026-03-15 13:42:49 -0400

    Initial CRAAIIGBORED pedalboard

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "pedalboard-cache-v5";
+const ASSETS_TO_CACHE = ["./", "./index.html", "./manifest.json"];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+
+            return Promise.resolve();
+          })
+        );
+      })
+    ])
+  );
+});
