4700a8fae2be72650952ba27f68ad637970414a3
Craig Chilcott <cjc244@gmail.com>
2026-03-16 01:20:28 -0400

    Add tuner chorus and spring reverb pedals

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pedalboard-cache-v5";
+const CACHE_NAME = "pedalboard-cache-v6";
 const ASSETS_TO_CACHE = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
