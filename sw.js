071513703d82bcd92a9ae5044a3ffd65c16a5a19
Craig Chilcott <cjc244@gmail.com>
2026-03-20 15:27:22 -0400

    Reduce live monitoring latency

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pedalboard-cache-v6";
+const CACHE_NAME = "pedalboard-cache-v8";
 const ASSETS_TO_CACHE = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
