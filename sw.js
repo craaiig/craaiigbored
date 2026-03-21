c1f97b0972d4cc3663af1f36f5b2510e2776c856
Craig Chilcott <cjc244@gmail.com>
2026-03-20 22:03:35 -0400

    Add keyboard shortcuts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pedalboard-cache-v8";
+const CACHE_NAME = "pedalboard-cache-v9";
 const ASSETS_TO_CACHE = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
